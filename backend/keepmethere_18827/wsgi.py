6eb5d3dadd88175262eed7719787e78c964738a6
Crowdbotics <team@crowdbotics.com>
2020-07-11 14:07:17 +0000

    Update models

--- backend/keepmethere_18827/wsgi.py
+++ backend/keepmethere_18827/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keepmethere_18827.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keepmethere_18827.settings")
 
 application = get_wsgi_application()
